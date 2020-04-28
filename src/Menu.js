6ae07ebf38b5e4b45cacc0969be73789b378e27d
jayanta <jayant-kumar.sahoo@hpe.com>
2020-04-28 20:13:08 +0530

    Comment Added

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,7 @@
 import {React} from 'react';
 import {ReactDOM} from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+//Check for Nav Links
 <Navbar bg="light" expand="lg">
   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
